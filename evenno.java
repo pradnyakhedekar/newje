9dda987698efd4b0aa74eb7734ae9d6bdb966f01
pradnyakhedekar <pradnyakhedekar99@gmail.com>
2022-05-08 23:51:09 +0530

    even

---
+++ evenno.java
@@ -0,0 +1,13 @@
+package Basiccommands;
+
+public class evennumbers {
+
+	public static void main(String[] args) 
+	{
+		for( int k= 1;k<=20;k++) 
+		 {
+		  if(k%2==0)  
+			  System.out.print("  "+k);
+		 } 
+	}
+}
